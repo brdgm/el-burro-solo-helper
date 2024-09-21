41d32cbc83db3952bae6473a6c552642a9aca7bf
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-09-21 10:52:15 +0200

    apply browserslist-to-esbuild, update browserslistrc a bit

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import { VitePWA } from 'vite-plugin-pwa'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
 import { description, appDeployName } from './package.json'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: `/${appDeployName}/`,
+  build: {
+    target: browserslistToEsbuild()
+  },
   plugins: [
     vue(),
     VitePWA({
